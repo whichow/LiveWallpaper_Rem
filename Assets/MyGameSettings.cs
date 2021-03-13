86cbe82c8d63d7b588ffe423f662012354c8b9d5
wuhao <wuhao@orbbec.com>
2021-03-13 16:49:58 +0800

    add settings and backgrounds

---
+++ Assets/MyGameSettings.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameSettings : MonoBehaviour 
+{
+	public AudioListener listener;
+	public new Light light;
+	public Material[] background;
+	public MeshRenderer bgRender;
+	public bool debug;
+
+	private int bgIndex;
+
+	public void ToggleVoice()
+	{
+		listener.enabled = !listener.enabled;
+	}
+
+	public void ToggleLight()
+	{
+		light.enabled = !light.enabled;
+	}
+
+	public void ChangeBackground()
+	{
+		bgIndex++;
+		if(bgIndex >= background.Length)
+		{
+			bgIndex = 0;
+		}
+		bgRender.material = background[bgIndex];
+	}
+
+	void OnGUI()
+	{
+		if(debug)
+		{
+			if(GUILayout.Button("Voice"))
+			{
+				ToggleVoice();
+			}
+			if(GUILayout.Button("Light"))
+			{
+				ToggleLight();
+			}
+			if(GUILayout.Button("Background"))
+			{
+				ChangeBackground();
+			}
+		}
+	}
+}
