68d2f5da45396bc266af92ea538cfe0d6c26b7da
wuhao <wuhao@orbbec.com>
2021-03-18 09:07:14 +0800

    add android project

---
+++ Android/src/main/java/com/wh/rem/LiveWallpaperService.java
@@ -0,0 +1,18 @@
+package com.wh.rem;
+
+import android.preference.PreferenceManager;
+
+import com.lostpolygon.unity.livewallpaper.UnityWallpaperService;
+
+/**
+ * Live Wallpaper Service. Override your stuff here.
+ */
+public class LiveWallpaperService extends UnityWallpaperService {
+    @Override
+    public void onCreate() {
+        // Load default preferences values
+        PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.preferences, false);
+
+        super.onCreate();
+    }
+}
